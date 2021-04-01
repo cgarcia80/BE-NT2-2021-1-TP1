77832d3d9a316ef5bb61fc2e7e3bfbc3e32f10fe
cgarcia80 <cesar.osc.garcia@gmail.com>
2021-04-01 11:12:42 -0300

    completo

--- script.js
+++ script.js
@@ -5,11 +5,48 @@ const classNames = {
     TODO_DELETE: 'todo-delete',
   }
   
+
   
   const list = document.getElementById('todo-list')
   const itemCountSpan = document.getElementById('item-count')
   const uncheckedCountSpan = document.getElementById('unchecked-count')
   
+  let contador = 0
+  let contadorChecked=0
+
   function addTodo() {
     alert('Boton Add TODO clickeado!')
+    const tituloTarea = prompt('Que tarea quieres agregar?')
+    if (tituloTarea != undefined){
+        contador++
+        itemCountSpan.innerHTML = contador
+        
+        var node = document.createElement("LI");
+        var check = document.createElement("INPUT");
+        var textnode = document.createTextNode(tituloTarea);
+
+        check.setAttribute("type", "checkbox");
+        check.className=classNames.TODO_CHECKBOX;
+        check.id ="data";
+        check.addEventListener( 'change', function() {
+          if(this.checked) {
+             //alert('checkbox esta seleccionado');
+             contadorChecked++;
+             uncheckedCountSpan.innerHTML=contadorChecked;
+          }
+          else{
+            contadorChecked--;
+            uncheckedCountSpan.innerHTML=contadorChecked;
+          }
+
+          node.appendChild(textnode);
+          node.className=classNames.TODO_ITEM
+          list.appendChild(node);
+          node.appendChild(check)
+
+      });
+       
+
+    }
+
   }
