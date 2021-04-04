73c5c577bf30987a322ee0a84a2a4a1e156b381f
AFelizzola <68514229+AFelizzola@users.noreply.github.com>
2021-04-03 21:07:36 -0300

    functions

--- script.js
+++ script.js
@@ -14,38 +14,50 @@ let contadorChecked = 0;
 
 function addTodo() {
   //alert('Boton Add TODO clickeado!')
-  const tituloTarea = prompt("Que tarea quieres agregar?");
-  if (tituloTarea != "") {
-    if (tituloTarea != undefined) {
-      contador++;
-      itemCountSpan.innerHTML = contador;
-      contadorChecked++;
-      uncheckedCountSpan.innerHTML=contadorChecked;
+  const tituloTarea = prompt("Qué tarea quieres agregar?");
+  tituloTarea.className = classNames.TODO_TEXT;
 
-      var node = document.createElement("LI");
+  if (tituloTarea != "") {
+    if (tituloTarea!= undefined) {
+
       var check = document.createElement("INPUT");
+      var node = document.createElement("LI");
       var textnode = document.createTextNode(tituloTarea);
 
-      check.setAttribute("type", "checkbox");
-      check.className = classNames.TODO_CHECKBOX;
-      check.id = "data";
-      agregarContador(check);
-      node.appendChild(textnode);
-      node.className = classNames.TODO_ITEM;
-      list.appendChild(node);
-      node.appendChild(check);
+      mostrarTarea(check, node, textnode, list);
+      contarTareasAgregadas();
+      contarUnchecked(check);
+
     }
   }
 }
 
-function agregarContador(check ){
+function mostrarTarea(check, node,textnode,list){    
+  //AGREGAMOS ESTILOS A LOS ELEMENTOS
+  check.setAttribute("type", "checkbox");
+  check.className = classNames.TODO_CHECKBOX;
+  node.className = classNames.TODO_ITEM;
+  list.appendChild(node);
+  node.appendChild(check);
+  node.appendChild(textnode); //INSERTA EN EL DOM   
+}
+
+function contarTareasAgregadas(){
+  contador++;
+  itemCountSpan.innerHTML = contador;
+}
+
+function contarUnchecked(check){
+  contadorChecked++;
+  uncheckedCountSpan.innerHTML = contadorChecked;
+  
   check.addEventListener("change", function () {
-  if (this.checked) {
-    //alert('checkbox esta seleccionado');
-    contadorChecked--;
-    uncheckedCountSpan.innerHTML = contadorChecked;
-  } else {
-    contadorChecked++;
-    uncheckedCountSpan.innerHTML = contadorChecked;
-  }
-})}
+    if (this.checked) {
+      //alert('checkbox esta seleccionado');
+      contadorChecked--;
+      uncheckedCountSpan.innerHTML = contadorChecked;
+    } else {
+      contadorChecked++;
+      uncheckedCountSpan.innerHTML = contadorChecked;
+  }})
+}
